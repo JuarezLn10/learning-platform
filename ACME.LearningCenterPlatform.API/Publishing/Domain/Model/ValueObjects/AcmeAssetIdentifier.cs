938b0977824988d6ea67dfdffbee4f1eb12b7b50
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-06 21:38:14 -0500

    feat(publishing): add publishing related value objects.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/ValueObjects/AcmeAssetIdentifier.cs
@@ -0,0 +1,12 @@
+﻿namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents an identifier for an asset in the ACME Learning Center Platform.
+/// </summary>
+/// <param name="Identifier">
+/// The unique identifier for the Asset.
+/// </param>
+public record AcmeAssetIdentifier(Guid Identifier)
+{
+    public AcmeAssetIdentifier() : this(Guid.NewGuid()) {}
+}
---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/ValueObjects/EPublishingStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents the various publishing statuses of a content item
+/// </summary>
+public enum EPublishingStatus
+{
+    Draft,
+    ReadyToEdit,
+    ReadyToApproval,
+    ApprovedAndLocked
+}
