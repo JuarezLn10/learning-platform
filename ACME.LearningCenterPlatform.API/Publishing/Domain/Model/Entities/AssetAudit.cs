0d4b6dea319049e234328bdb0d517750c1c59b0e
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-06-06 21:59:58 -0500

    feat(assets): add asset type.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/AssetAudit.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public partial class Asset
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/ImageAsset.cs
@@ -0,0 +1,23 @@
+﻿using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public class ImageAsset(EAssetType Type) : Asset(Type)
+{
+    private Uri? ImageUri { get; }
+    
+    public override bool Readable => false;
+    public override bool Viewable => true;
+    
+    public ImageAsset() : this(EAssetType.Image) { }
+
+    public ImageAsset(string imageUrl) : this(EAssetType.Image)
+    {
+        ImageUri = new Uri(imageUrl);
+    }
+
+    public override string GetContent()
+    {
+        return ImageUri != null ? ImageUri.AbsoluteUri : string.Empty;
+    }
+}
